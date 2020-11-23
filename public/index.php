19ff21aff5bfdaa04ed6e31e820a652982c8f13d
liandi <simdigliandi@gmail.com>
2020-11-23 08:56:23 +0700

    first commit

--- public/index.php
+++ public/index.php
@@ -5,11 +5,11 @@ define('ROOT', dirname(__FILE__));
 define('DS', DIRECTORY_SEPARATOR);
 
 // now require file
-require_once "config.php";
-require_once "library/database.class.php";
-require_once "library/model.class.php";
-require_once "library/view.class.php";
-require_once "library/controller.class.php";
+require_once "../config/config.php";
+require_once "../library/database.class.php";
+require_once "../library/model.class.php";
+require_once "../library/view.class.php";
+require_once "../library/controller.class.php";
 
 function __autoload($className) {
 
